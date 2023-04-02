d043761ad4d676ac1e5d0e50742c54ca9f029e07
Ömer Ünal <mrfrknl3232@gmail.com>
2023-04-02 14:51:27 +0200

    erdal1

--- src/B02.java
+++ src/B02.java
@@ -1,5 +1,8 @@
 public class B02 {
     public static void main(String[] args) {
-
+        /*
+        git branch murat  , branc #h olusturur
+        git checkout murat ,  muratin branch'ine gider
+         */
     }
 }
---
+++ src/B04.java
@@ -0,0 +1,5 @@
+public class B04 {
+    public static void main(String[] args) {
+        System.out.println("erdal calismaya baslasin");
+    }
+}
