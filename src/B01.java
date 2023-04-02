32b47c75d3e175283591a77dda808f65c84f77fd
Ömer Ünal <mrfrknl3232@gmail.com>
2023-04-02 14:44:23 +0200

    1.versiyon

---
+++ src/B01.java
@@ -0,0 +1,6 @@
+public class B01 {
+    public static void main(String[] args) {
+        System.out.println("Branch Kullanimi");
+
+    }
+}
---
+++ src/B02.java
@@ -0,0 +1,5 @@
+public class B02 {
+    public static void main(String[] args) {
+
+    }
+}
